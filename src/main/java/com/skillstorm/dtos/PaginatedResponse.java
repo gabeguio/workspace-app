2ca1895684c68ad02323c14ec2b713da6198e89c
gabeguio <gabe.guio@gmail.com>
2024-06-27 21:53:21 -0400

    Moved paginated response logic from controller to service

--- src/main/java/com/skillstorm/dtos/PaginatedResponse.java
+++ src/main/java/com/skillstorm/dtos/PaginatedResponse.java
@@ -10,14 +10,14 @@ public class PaginatedResponse<T> {
     private long totalElements;
     private int totalPages;
     private boolean last;
-    private List<Employee> content;
+    private List<T> content;
 
 	public PaginatedResponse() {
 		super();
 	}
 
 	public PaginatedResponse(int pageNumber, int pageSize, long totalElements, int totalPages, boolean last,
-			List<Employee> content) {
+			List<T> content) {
 		super();
 		this.pageNumber = pageNumber;
 		this.pageSize = pageSize;
@@ -31,58 +31,47 @@ public class PaginatedResponse<T> {
 		return pageNumber;
 	}
 
-
 	public void setPageNumber(int pageNumber) {
 		this.pageNumber = pageNumber;
 	}
-
 
 	public int getPageSize() {
 		return pageSize;
 	}
 
-
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 	}
-
 
 	public long getTotalElements() {
 		return totalElements;
 	}
 
-
 	public void setTotalElements(long totalElements) {
 		this.totalElements = totalElements;
 	}
-
 
 	public int getTotalPages() {
 		return totalPages;
 	}
 
-
 	public void setTotalPages(int totalPages) {
 		this.totalPages = totalPages;
 	}
-
 
 	public boolean isLast() {
 		return last;
 	}
 
-
 	public void setLast(boolean last) {
 		this.last = last;
 	}
 
-
-	public List<Employee> getContent() {
+	public List<T> getContent() {
 		return content;
 	}
 
-
-	public void setContent(List<Employee> content) {
+	public void setContent(List<T> content) {
 		this.content = content;
 	}
 
